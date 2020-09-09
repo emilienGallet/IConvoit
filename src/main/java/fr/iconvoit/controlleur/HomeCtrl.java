0da2559b93f5916324ee13b68ce8557dc2cf1218
emilien <emilien@emilien-VirtualBox>
2020-09-09 22:05:09 +0200

    .jsp => thymeleaf. Mise en place de l'index. Add H2,TL,Lombok in pom.xml

--- src/main/java/fr/iconvoit/controlleur/HomeCtrl.java
+++ src/main/java/fr/iconvoit/controlleur/HomeCtrl.java
@@ -1,5 +1,15 @@
 package fr.iconvoit.controlleur;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeCtrl {
+	@RequestMapping(path = {"/","/home"})
+	public String index() {
+		//TODO: process PUT request
+		
+		return "index";
+	}
 
 }
