b1299df5f6ea94c89d647c95b6502366c6ad05d1
SuperChris242 <okoueretchristian@gmail.com>
2020-09-24 09:16:37 +0200

    Fix Localisation
    update of the localization class,
    basically I modified to separate the location identity into two real latitude and longitude and create methods that go with it.

--- src/main/java/fr/iconvoit/entity/Localisation.java
+++ src/main/java/fr/iconvoit/entity/Localisation.java
@@ -14,6 +14,21 @@ public class Localisation {
 	@NotNull
 	private String nameLocation;
 	private ArrayList<Localisation> subLocalisation;
+	private float longitude;
+	private float latitude;
+
+	public Localisation(){
+
+	}
+
+	public Localisation(String name, float longitude,float latitude){
+		this.nameLocation = name;
+		if(longitude == 0 && latitude == 0)
+			System.err("Erreur de Syntaxe");
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+
 
 	public ArrayList<Localisation> getSubLocalisation() {
 		return subLocalisation;
@@ -29,5 +44,27 @@ public class Localisation {
 
 	public void setNameLocation(String nameLocation) {
 		this.nameLocation = nameLocation;
+	}
+
+	
+	public String toString(){
+		return "Coordinate of"+this.getNameLocation() +
+		" is "+ "this.getLongitude() ." + "this.getLatitude()";
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
 	}
 }
